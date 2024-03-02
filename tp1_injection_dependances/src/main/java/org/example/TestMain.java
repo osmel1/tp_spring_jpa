83a840fb9d59f2663664232e379db8eb76ced69d
osmel1 <elhaoussama483@gmail.com>
2024-03-02 14:54:28 +0100

    Initial

--- tp1_injection_dependances/src/main/java/org/example/TestMain.java
+++ tp1_injection_dependances/src/main/java/org/example/TestMain.java
@@ -4,16 +4,17 @@ import org.example.dao.IDao;
 import org.example.metier.IMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("Injection de dependances par annotations");
+        System.out.println("Injection de dependances par XML");
         // lire le fichier de configuration Spring format Java
-        ApplicationContext context = new AnnotationConfigApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
 
         // récupérer le bean metier
-        IDao metier = (IDao) context.getBean("base");
+        IMetier metier =(IMetier) context.getBean("metier");
         // Pour tester
-        //System.out.println(metier.calcul());
+        System.out.println(metier.calcul());
     }
 }
--- tp1_injection_dependances/src/main/java/org/example/presentationDyn/Main.java
+++ tp1_injection_dependances/src/main/java/org/example/presentationDyn/Main.java
@@ -14,8 +14,6 @@ public class Main {
 
             //lire le fichier config.txt
             Scanner scanner = new Scanner(new File("config.txt"));
-
-
             /*pour Dao*/
             //lire le nom de la classe Dao depuis le fichier config.txt
             String daoClassName = scanner.nextLine();
@@ -24,25 +22,21 @@ public class Main {
             IDao dao = (IDao) cDao.newInstance();
             //afficher le résultat
             System.out.println(dao.getDate());
-
             /*pour Metier*/
             //lire le nom de la classe Dao depuis le fichier config.txt
             String metierClassName = scanner.nextLine();
             //charger la classe Dao
             Class cMetier = Class.forName(metierClassName);
             IMetier metier = (IMetier) cMetier.newInstance();
-
             //récupérer la méthode setDao
             Method setDaoMethod = cMetier.getMethod("setDao", IDao.class);
             //appeler la méthode setDao
             setDaoMethod.invoke(metier, dao);
             //afficher le résultat
             System.out.println(metier.calcul());
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
 
     }
 }
